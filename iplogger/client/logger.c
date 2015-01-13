dce7e5455cdeeb3f5d6f719c4e2b693348011fbf
Ajay Vishnu <avishnu@sapient.com>
2015-01-13 13:44:36 +0530

    linux intervals

--- iplogger/client/logger.c
+++ iplogger/client/logger.c
@@ -7,6 +7,7 @@
 #define MAX_NAME 32
 #define MAX_IP 39
 #define DEFAULT_SERVER "http://0th.me/iplogger/"
+#define DEFAULT_INTERVAL 1000*60*50
 
 int curl(char *server, char *name, char *ip) {
   CURL *curl;
@@ -38,29 +39,31 @@ int main(int argc, char *argv[])
 {
   FILE *fp;
   char str[1035];
-
-  fp = popen("ifconfig", "r");
-  if (fp == NULL) {
-    printf("Failed to run command\n" );
-    exit(1);
-  }
-
   char name[MAX_NAME], ip[MAX_IP];
-  char * server = argc>1? argv[1]: DEFAULT_SERVER; 
+  char * server;
+  int hostLen;
     
-  int hostLen = gethostname(name, MAX_NAME);
-  name[strlen(name)] = '_';
-    
-  while (fgets(str, sizeof(str), fp) != NULL) {
-    if(str[0] != '\t') {
-        memcpy(name + strlen(name), strtok(str,":"), MAX_NAME);
-    }
-    else if(!memcmp(str, "\tinet ",6)) {
-        memcpy(ip, strtok(str+5, " "),MAX_IP);
-        curl(server, name, ip);
-    }
-  }
+  while(1) {    
 
-  pclose(fp);
+      fp = popen("ifconfig", "r");
+      if (fp == NULL) {
+        printf("Failed to run command\n" );
+        exit(1);
+      }
+
+      server = argc>1? argv[1]: DEFAULT_SERVER; 
+      gethostname(name, MAX_NAME);
+      name[hostLen = strlen(name)] = '_';
+      while (fgets(str, sizeof(str), fp) != NULL) 
+        if(str[0] != '\t') 
+            memcpy(name + hostLen + 1, strtok(str,":"), MAX_NAME);
+        else if(!memcmp(str, "\tinet ",6)) 
+            curl(server, name, memcpy(ip, strtok(str+5, " "),MAX_IP));
+
+      pclose(fp);
+          
+      sleep(DEFAULT_INTERVAL);      
+  }
+      
   return 0;
 }
